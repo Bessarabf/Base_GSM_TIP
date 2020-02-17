8973714ef3066460a9de7d7541b5870239eb4c5d
faustus-mihail <zetamiha@bk.ru>
2020-02-17 16:09:50 +0300

    DS BDIP

--- Source/ds.for
+++ Source/ds.for
@@ -1,5 +1,12 @@
-      function ds(altm,alt,altp,tetm,tetp)
-      data re/6371.02e5/
-      ds=sqrt((altp-altm)**2+(alt+re)**2*(tetp-tetm)**2)
-      return
-      end
+!      function ds(altm,alt,altp,tetm,tetp)
+!      data re/6371.02e5/
+!      ds=sqrt((altp-altm)**2+(alt+re)**2*(tetp-tetm)**2)
+!      return
+!      end
+! Updated Mihail Melnik from Polar Geophysical Institute 14.02.2020
+       function ds(altm,alt,altp,tetm,tetp)
+       real,parameter :: re = 6371.02e5
+        real altm,alt,altp,tetm,tetp
+       ds=sqrt((altp-altm)*(altp-altm)+(alt+re)*(alt+re)*(tetp-tetm)*
+     *  (tetp-tetm))
+       end
