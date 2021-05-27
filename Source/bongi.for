07e1055644cfdb65507c761ee5782ff18aeb617a
Faustus-mihail <54106820+Faustus-mihail@users.noreply.github.com>
2021-05-27 18:32:17 +0300

    возвращение маленьких букв

--- Source/bongi.for
+++ Source/bongi.for
@@ -1,27 +1,25 @@
       subroutine bongi(gins,ins,nh,its,ids)
       dimension gins(ins,nh,its,ids)
-      i2=its-1
-      do 3 i=1,3
-          np=i
-          if(i.eq.2)np=5
-          if(i.eq.3)np=6
-       do 2 k = 1 , nh
+      i2 = its - 1
+      do i = 1, 3
+        np = i
+        if(i == 2) np = 5
+        if(i == 3) np = 6
+        do  k = 1 , nh
 c      ssp - sum s.pole
 c      snp - sum n.pole
-        s np=0.
-        s sp=0.
-        do 1 j = 1 , ids
-         snp=snp+gins(np,k,2,j)
-         ssp=ssp+gins(np,k,i2,j)
-   1    continue
-c
-        unp=snp/ids
-        usp=ssp/ids
-        do 4 j=1,ids
-          gins(np,k,1,j)=unp
-          gins(np,k,its,j)=usp
-    4   continue
-    2  continue
-    3 continue
-      return
+          snp = 0.
+          ssp = 0.
+          do j = 1, ids
+            snp = snp + gins(np,k,2,j)
+            ssp = ssp + gins(np,k,i2,j)
+          enddo ! j
+          unp = snp / ids
+          usp = ssp / ids
+          do j = 1, ids
+            gins(np,k,1,j) = unp
+            gins(np,k,its,j) = usp
+          enddo ! j
+        enddo ! k
+      enddo ! i
       end
