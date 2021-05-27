ad45fa422d33078280ae4f481bd85e5de6d72c5b
Faustus-mihail <54106820+Faustus-mihail@users.noreply.github.com>
2021-05-27 18:36:31 +0300

    изменения в запускающем файле

--- zapusk.bat
+++ zapusk.bat
@@ -2,10 +2,11 @@
 :: ifort /c /assume:byterecl /Od /check:all /debug:full *.for 
 
 ifort /c Source\mo_bas_gsm.f90
-::ifort /c /assume:byterecl /Od /extend_source:80 /fpp /Qopenmp  /traceback /check:bounds  *.for 
-ifort /c /assume:byterecl /extend_source:80 /fpp /traceback /check:bounds  Source\*.for 
-:: ifort *.obj /link /OUT:"GSM_TIP.exe" 
-ifort *.obj
+:: ifort /c /assume:byterecl /Od /extend_source:80 /fpp /traceback /check:bounds  /check:all Source\*.for 
+:: ifort /c /assume:byterecl /extend_source:80 /fpp /traceback  /Qfp-speculation=off /fpe=0      Source\*.for
+   ifort /c /assume:byterecl /extend_source:80 /fpp /traceback  /Qfp-speculation=off /fpe=0 /Od  Source\*.for 
+ifort *.obj /link /OUT:"GSM_TIP.exe" 
 del *.obj
 del mo_bas_gsm.mod
-aepp.exe
+GSM_TIP.exe >output_d.txt
+::GSM_TIP.exe
